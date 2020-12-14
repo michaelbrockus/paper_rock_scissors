a58864ea5dac617bfdc7bbd8ef5b4511e9ab7e97
Michael <michaelbrockus@gmail.com>
2020-12-13 17:27:23 -0800

    write paper rock scissors game

--- code/program.d
+++ code/program.d
@@ -4,16 +4,9 @@
 // gmail: <michaelbrockus@gmail.com>
 //
 module program;
+import game;
 import std.stdio;
 
-
-//
-// Greet the user
-//
-string greet()
-{
-    return "Hello, D Developer.";
-} // end of functions greet
 
 //
 // foundation of the program and related
@@ -22,5 +15,49 @@ string greet()
 //
 void foundation()
 {
-    writefln("%s", greet());
+    int rock = 1;
+    int paper = 1;
+    int scissors = 1;
+ 
+    while (true)
+    {
+        write("rock, paper or scissors? ");
+        immutable weapon = readln.strip.toLower;
+        if (weapon.empty)
+        {
+            break;
+        } // end if
+ 
+        immutable player = weapon.maybeTo!Choice;
+        if (h.isNull)
+        {
+            writeln("Wrong input: ", weapon);
+            continue;
+        } // end if
+ 
+        immutable computer = genMove(rock, paper, scissors);
+        writeln("Player: ", player.get, " Computer: ", computer);
+ 
+        if (beats(player, computer))
+        {
+            writeln("Player wins\n");
+        } // end if
+        else if (beats(computer, player))
+        {
+            writeln("Player loses\n");
+        } // end else if
+        else
+        {
+            writeln("Draw\n");
+        } // end else
+ 
+        final switch (player.get)
+        {
+            case Choice.rock:     r++; break;
+            case Choice.paper:    p++; break;
+            case Choice.scissors: s++; break;
+        } // end switch
+
+    } // end while
+
 } // end of function foundation
