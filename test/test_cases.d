d762249d0c813bf9d7885a3da455740a407d5ac2
Michael <michaelbrockus@gmail.com>
2020-12-13 18:24:36 -0800

    write the test cases

--- test/test_cases.d
+++ test/test_cases.d
@@ -10,25 +10,27 @@
 // of common test cases
 //
 module test_cases;
-import program;
+import game;
 
 
 //
 // list of all the test cases for this project
 //
+
+// [test_beatsPlayerWinCases]
 unittest
 {
-    assert(true);
+    assert(beats(Choice.paper, Choice.rock) == true);
+    assert(beats(Choice.scissors, Choice.paper) == true);
+    assert(beats(Choice.rock, Choice.scissors) == true);
 } // end of test case
 
+// [test_beatsComputerWinCases]
 unittest
 {
-    assert("Hello, D Developer." == greet());
-} // end of test case
-
-unittest
-{
-    assert(greet() != null);
+    assert(beats(Choice.rock, Choice.paper) == false);
+    assert(beats(Choice.paper, Choice.scissors) == false);
+    assert(beats(Choice.scissors, Choice.rock) == false);
 } // end of test case
 
 
